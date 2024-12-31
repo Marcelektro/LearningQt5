3f33b7744db89667791a14206e9967c61e36bf25
Marcelektro <marcelektro17@gmail.com>
2024-12-31 16:51:01 +0100

    Updated build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# Build the project using cmake
-cd ../
+set -e # exit on errors
 
-mkdir -p dist
+# Build the project using cmake
+SCRIPT_ABSOLUTE_PATH=$(readlink -f "$0")
+PROJECT_ROOT=$(dirname "$SCRIPT_ABSOLUTE_PATH")/../
+
+echo "Project root: $PROJECT_ROOT"
+
+cd "$PROJECT_ROOT"
+
+
+mkdir -p ./dist
 
 cmake -S . -B ./dist/
 cmake --build ./dist/
